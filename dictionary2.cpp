1df2d4c85d2f167b561becf9ccbbcc8b97242e82
Paul DiOrio <none>
2015-11-11 23:51:45 -0500

    Added proper ICU interger->utf8 formatting

--- dictionary2.cpp
+++ dictionary2.cpp
@@ -1,19 +1,20 @@
 #include <unicode/unistr.h>
 #include <unicode/ustream.h>
 #include <unicode/regex.h>
+#include <unicode/numfmt.h>
 #include <iostream>
 #include <fstream>
 #include <map>
 
-std::map<int, UnicodeString> read_lines(std::string filename)
+std::map<int, icu::UnicodeString> read_lines(std::string filename)
 {
 	// initialize lookup dictionary
-	std::map<int, UnicodeString> dictionary;
+	std::map<int, icu::UnicodeString> dictionary;
 
 	// regex setup; split on whitespace upto a max of 3 per line
 	// first field is integer number, second field is word, the rest of the line is stored in third field
 	UErrorCode status = U_ZERO_ERROR;
-	RegexMatcher m("\\s+", 0, status); 
+	icu::RegexMatcher m("\\s+", 0, status); 
 	const int maxWords = 3;
 
 	// setup file
@@ -31,10 +32,10 @@ std::map<int, UnicodeString> read_lines(std::string filename)
 	while (std::getline(words_file, line))
 	{
 		// convert line to ICU unicode string
-		UnicodeString ucs = UnicodeString::fromUTF8(StringPiece(line.c_str()));
+		icu::UnicodeString ucs = icu::UnicodeString::fromUTF8(StringPiece(line.c_str()));
 
 		// initialize array of unicodestrings, upto max set previously
-		UnicodeString words[maxWords];    
+		icu::UnicodeString words[maxWords];    
     	
 		// split string into components
 		int numWords = m.split(ucs, words, maxWords, status);
@@ -64,11 +65,21 @@ int main(int argc, char* argv[])
 	std::string filename = argv[1];
 
 	// parse lines from filename into a dictionary mapping from int->word
-	std::map<int, UnicodeString> dict = read_lines(filename);
+	std::map<int, icu::UnicodeString> dict = read_lines(filename);
+
+	UErrorCode success = U_ZERO_ERROR;
+	icu::NumberFormat *nf = icu::NumberFormat::createInstance(success);
+	// seems to run faster when not absolutely enforcing min/max fractional digits
+	//nf->setMinimumFractionDigits(0);
+    //nf->setMaximumFractionDigits(0);
+    nf->setGroupingUsed(false);
 
 	// display entire map using iterators
-	for (std::map<int, UnicodeString>::iterator it = dict.begin(); it != dict.end(); ++it) {
-		std::cout << it->first << ": " << it->second << std::endl;
+	for (std::map<int, icu::UnicodeString>::iterator it = dict.begin(); it != dict.end(); ++it) {
+		//std::cout << it->first << ": " << it->second << std::endl;
+
+		icu::UnicodeString temp_ustring;
+		std::cout << nf->format(it->first, temp_ustring) << ": " << it->second << std::endl;
 	}
 
 	return 0;
